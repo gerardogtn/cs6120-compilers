f3f4803b28ae185777221e9aa64aa2932ef39263
Gerardo Teruel <gerardogtn@gmail.com>
2025-02-03 20:32:14 -0500

    improve json parsing and add boilerplate for lesson 3

---
+++ tasks3/build.sh
@@ -0,0 +1,10 @@
+KOTLINC=./lib/kotlinc/bin/kotlinc
+
+MOSHI_KOTLIN_JAR=./lib/moshi-kotlin.jar
+MOSHI_JAR=./lib/moshi.jar
+OKIO_JAR=./lib/okio.jar
+
+kotlinc \
+        -cp $MOSHI_JAR:$OKIO_JAR:$MOSHI_KOTLIN_JAR \
+        -d build \
+        *.kt
